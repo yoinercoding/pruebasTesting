c7ea8c326d4ad57cc1b3ab9e1f4d5f80928a327f
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-06-14 09:34:39 -0300

    first commit

---
+++ src/test/java/MoneyUtilTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MoneyUtilTest {
+
+    @Test
+    public void moneyTest() {
+        String money = MoneyUtil.format(1000);
+        Assert.assertEquals("$1000.00", money);
+    }
+
+    @Test
+    public void negativeMoneyTest() {
+        String money = MoneyUtil.format(-1000);
+        Assert.assertEquals("-$1000.00", money);
+    }
+
+    @Test
+    public void euroMoneyTest() {
+        String money = MoneyUtil.format(-1000,  "€");
+        Assert.assertEquals("-€1000.00", money);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void notNullExceptionMoneyTest() {
+        String money = MoneyUtil.format(-1000,  null);
+
+    }
+}
